fe3b3befd0e1134962bf805b42b866a792891e84
Jakub Skoczylas <jakub.skoczylas@gmail.com>
2018-01-12 18:02:22 +0100

    partials, and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,12 @@
 class ArticlesController < ApplicationController
 
+
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
-
   end
 
   def create
@@ -20,6 +24,28 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was succssfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was succssfully deleted"
+    redirect_to articles_path
+  end
+
 
 
   private
